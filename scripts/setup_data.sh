a9c7a5ad46dd80344c7f09dc1647b01263da1c12
Pax-pi <pang.ax@hotmail.com>
2026-05-15 18:39:54 -0700

    feat: add automated Kaggle dataset download shell script

---
+++ scripts/setup_data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# step 1, check the environment variable
+if [ -z "$KAGGLE_API_TOKEN" ]; then
+    echo "Error: Kaggle API Token has not been set."
+    exit 1
+fi
+
+# step 2, check  kaggle is installed
+if command -v kaggle &> /dev/null; then
+    :
+else
+    echo "Kaggle is not installed, installing Kaggle through pip."
+    pip install kaggle
+fi
+
+# step 3, use kaggle to download the dataset
+mkdir -p data/rsna
+cd data/rsna
+kaggle competitions download -c rsna-pneumonia-detection-challenge
+
+# step 4, unzip it
+
+unzip -q rsna-pneumonia-detection-challenge.zip
+
+# step 5, delete the zip file
+
+rm rsna-pneumonia-detection-challenge.zip
